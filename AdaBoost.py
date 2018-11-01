0ef03b00552df086821dba2f0672a008477b45fe
gracemoon <15921991927@163.com>
2018-11-01 22:52:06 +0800

    update AdaBoost.py

---
+++ AdaBoost.py
@@ -0,0 +1,31 @@
+# 准备数据
+def load_data():
+    result = []
+    return result
+
+
+# 生成单层决策树
+def simple_layer_decision():
+    result = []
+    return result
+
+
+# 根据阈值分类
+def judge():
+    result = []
+    return result
+
+
+# AdaBoost算法实现
+def adaptive_boost():
+    result = []
+    return result
+
+
+# 更新alpha、更新权重w
+def update_weight():
+    result = []
+    return result
+
+# 测试
+def test():
