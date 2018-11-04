709f907b67b1208fbe799b18c3a13859e568dac1
gracemoon <15921991927@163.com>
2018-11-04 18:47:28 +0800

    update K-means.py

--- K-means.py
+++ K-means.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 # 准备数据
 file_path = 'D:\Download\machinelearninginaction\Ch10\\testSet.txt'
@@ -50,12 +51,13 @@ def choose_cluster(martix):
     temp = []
     for k in range(len(martix)):
         temp.append(martix[k][0])
+    for m in range(len(martix[0])):
         result.append(0)
     for i in range(len(martix[0])):
         for j in range(len(martix)):
             if martix[j][i] < temp[j]:
                 temp[j] = martix[j][i]
-                result[j] = j
+                result[i] = j
     return result
 
 
@@ -63,6 +65,77 @@ def choose_cluster(martix):
 def centroid(dataset, vector, n):
     temp = []
     for i in range(len(dataset[0])):
-        temp_i = [0]*n
-        for j in vector:
-            temp_i[j] += dataset[i][j]
+        temp_i = [0] * n
+        sum = [0] * n
+        for j in range(len(dataset)):
+            temp_i[vector[j]] += dataset[j][i]
+            sum[vector[j]] += 1
+        for k in range(len(temp_i)):
+            if sum[k] != 0:
+                temp_i[k] /= sum[k]
+            else:
+                print(temp_i[k])
+        temp.append(temp_i)
+    result = []
+    for l in range(len(temp[0])):
+        temp_result = []
+        for t in range(len(temp)):
+            temp_result.append(temp[t][l])
+        result.append(temp_result)
+    return result
+
+
+# 判断簇类别不再变化
+def judge_cluster(first, second):
+    result = 1
+    for i in range(len(first)):
+        if first[i] != second[i]:
+            result = 0
+            break
+    return result
+
+
+# 随机生成初始质心
+def random_centroid(m, n):
+    result = []
+    while len(result) < m:
+        flag = 0
+        temp = int(random.uniform(0, n))
+        if len(result) > 0:
+            for i in range(len(result)):
+                if temp == result[i]:
+                    flag = 1
+            if flag == 0:
+                result.append(temp)
+        else:
+            result.append(temp)
+    return result
+
+
+# K-means算法实现
+def k_means(dataset, num):
+    centroid_init = random_centroid(num, len(dataset))
+    init_nodes = []
+    for item in centroid_init:
+        init_nodes.append(dataset[item])
+    over_flag = 1
+    old_cluster = [-1] * len(dataset)
+    while over_flag:
+        distince_martix = distince(dataset, init_nodes)
+        clusters = choose_cluster(distince_martix)
+        judge_flag = judge_cluster(old_cluster, clusters)
+        if judge_flag == 1:
+            over_flag = 0
+        else:
+            old_cluster = clusters
+            init_nodes = centroid(dataset, clusters, num)
+    return init_nodes, clusters
+
+
+def test():
+    cluster_num = 4
+    test_dataset = un_labels_data(file_path)
+    print(k_means(test_dataset, cluster_num))
+    
+
+test()
