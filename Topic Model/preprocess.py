2ee8027f38af1393765fe30612a113c0c7efe3b6
liang xu <15921991927@163.com>
2020-05-15 22:44:11 +0800

    update

--- Topic Model/preprocess.py
+++ Topic Model/preprocess.py
@@ -1,5 +1,37 @@
-import numpy 
-import pandas 
+import numpy as np
+import pandas as pd
+from gensim.corpora.dictionary import Dictionary
+import jieba
 
+class Preprocess(object):
+    def reader(self,path):
+        self.data=pd.read_csv(path,encoding='utf-8')
+        self.preprocess()
+        self.createCorpus()
 
-class 
+    def preprocess(self):
+        # 加载停用词
+        self.stopwords=set()
+        with open("Topic Model/data/stopwords.txt",encoding='utf-8') as file:
+            for line in file:
+                self.stopwords.add(line[:-1])
+        
+        # 去除空行
+        self.data.dropna(inplace=True)
+        # 分词
+        self.sentences=[]
+        for line in self.data['content'].values.tolist():
+            try:
+                sentence=jieba.lcut(line)
+                # 去除空格、数字、停用词    
+                sentence=list(filter(lambda x: x.strip(),sentence))
+                sentence=list(filter(lambda x: not str(x).isdigit(),sentence))
+                sentence=list(filter(lambda x: x not in self.stopwords,sentence))
+                self.sentences.append(sentence)
+            except Exception:
+                print('1')
+                continue
+
+    def createCorpus(self):
+        self.dictionary=Dictionary(self.sentences)
+        self.corpus=[self.dictionary.doc2bow(sentence) for sentence in self.sentences]
--- Topic Model/topicModel.py
+++ Topic Model/topicModel.py
@@ -1,1 +1,47 @@
 import gensim 
+from gensim.models import LdaModel,TfidfModel
+from preprocess import Preprocess
+from sklearn.model_selection import train_test_split
+import numpy as np 
+import pandas as pd
+
+
+class TopicModel(object):
+    def dataPreprocess(self,path):
+        self.preprocess=Preprocess()
+        self.preprocess.reader(path)
+
+    def train(self):
+        self.lda=LdaModel(self.preprocess.corpus,id2word=self.preprocess.dictionary,num_topics=10)
+        for topic in self.lda.print_topics(num_topics=10,num_words=10):
+            print(topic[1])
+
+    def evaluation(self):
+        pass
+
+class TFIDFModel(object):
+    def dataPreprocess(self,path):
+        self.preprocess=Preprocess()
+        self.preprocess.reader(path)
+
+    def train(self):
+        self.tfidf=TfidfModel(corpus=self.preprocess.corpus,id2word=self.preprocess.dictionary)
+        data=self.tfidf[self.preprocess.corpus[0]]
+        df=pd.DataFrame(np.array(data),columns=['index','tfidf'])
+        df.sort_values(by='tfidf',ascending=False,inplace=True)
+        id2token={}
+        for key,value in self.preprocess.dictionary.token2id.items():
+            id2token[value]=key
+        print([id2token[x] for x in df['index'].values])
+
+    def evaluation(self):
+        pass
+
+
+if __name__ == "__main__":
+    # model=TopicModel()
+    model=TFIDFModel()
+    model.dataPreprocess("Topic Model/data/car.csv")
+    model.train()
+
+
